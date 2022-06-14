9a91be45d7f3f764b5086c9b955399f4658e2e00
mollameehedi <mollameehedi@gmail.com>
2022-06-14 06:36:28 +0600

    Create Profile Model and Router

--- middlewares/routes.js
+++ middlewares/routes.js
@@ -2,6 +2,7 @@ const userRouter = require('../routers/userRouter');
 const categoryRouter = require('../routers/categoryRouter');
 const productRouter = require('../routers/productRouter');
 const cartRouter = require('../routers/cartRouter');
+const profileRouter = require('../routers/profileRouter');
 
 
 module.exports = (app) =>{
@@ -9,4 +10,5 @@ module.exports = (app) =>{
     app.use('/api/category', categoryRouter);
     app.use('/api/product',productRouter);
     app.use('/api/cart',cartRouter);
+    app.use('/api/profile',profileRouter);
 }
