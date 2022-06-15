ce5c7361fb2fb20d122306b39f620911018eb6d5
mollameehedi <mollameehedi@gmail.com>
2022-06-16 04:41:34 +0600

    Private Route i mean Router authentication

--- ecom-frontend/src/components/Main.js
+++ ecom-frontend/src/components/Main.js
@@ -2,6 +2,8 @@ import {Switch,Route,Redirect}  from 'react-router-dom';
 import Home from './home/Home';
 import Login from './user/Login';
 import Register from './user/Register';
+import PrivateRoute from './protectedRoutes/PrivateRoute';
+import Dashboard from './user/Dashboard';
 
 const Main = () => {
     return (
@@ -10,6 +12,9 @@ const Main = () => {
                 <Route path='/' exact component={Home}/>
                 <Route path='/login' exact component={Login}/>
                 <Route path='/register' exact component={Register}/>
+                <PrivateRoute path='/dashboard'>
+                        <Dashboard/>
+                </PrivateRoute>
         </Switch>
     </div>
     )
