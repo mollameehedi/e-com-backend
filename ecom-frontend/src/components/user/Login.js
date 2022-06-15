39f975bd5ec7c936124b21922589f26bb380104b
mollameehedi <mollameehedi@gmail.com>
2022-06-16 02:45:04 +0600

    User authenticated through JSON web token

--- ecom-frontend/src/components/user/Login.js
+++ ecom-frontend/src/components/user/Login.js
@@ -3,6 +3,7 @@ import Layout from '../Layout';
 import { showError, showLoading} from '../../utils/messages';
 import {login} from '../../api/apiAuth';
 import {Redirect} from 'react-router-dom';
+import { authenticate } from '../../utils/auth';
 
 const Login = () => {
     const [values, setValues] = useState({
@@ -33,14 +34,17 @@ const Login = () => {
             });
             login({email,password})
             .then(response =>{
-                setValues({
-                    email: '',
-                    password: '',
-                    success:true,
-                    disabled:false,
-                    loading:false,
-                    redirect:true,
+                authenticate(response.data.token, () =>{
+                    setValues({
+                        email: '',
+                        password: '',
+                        success:true,
+                        disabled:false,
+                        loading:false,
+                        redirect:true,
+                    })
                 })
+                
             })
             .catch(err =>{
                 let errMsg = 'Something went wrong!';
