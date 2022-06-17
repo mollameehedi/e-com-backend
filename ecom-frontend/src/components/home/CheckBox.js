5c0ccaaa61b57b552773eebc3412fd0d0ac97ebb
mollameehedi <mollameehedi@gmail.com>
2022-06-17 21:34:33 +0600

    Filter Product by category part 2

--- ecom-frontend/src/components/home/CheckBox.js
+++ ecom-frontend/src/components/home/CheckBox.js
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 
 const CheckBox = ({categories}) =>{
+    const [checked, setChecked] = useState([]);
+    const checkedIds = [...checked];
+
+
+    const handleToggle = id=> () =>{
+        const foundId = checked.indexOf(id);
+        if(foundId === -1){
+            checkedIds.push(id);
+        }else{
+            checked.splice(foundId,1);
+        }
+        setChecked(checkedIds);
+    }
+
+    useEffect(() =>{
+        alert(JSON.stringify(checked))
+    },[checked])
+
     return categories.map(category =>(
         <li className="list-unstyled" key={category._id}>
             <input 
+                onChange={handleToggle(category._id)}
                 type='checkbox'
                 className='form-check-input'
+                value={checked.indexOf(category._id === -1)}
             />
             <lable className='form-check-label'>{category.name}</lable>
         </li>
